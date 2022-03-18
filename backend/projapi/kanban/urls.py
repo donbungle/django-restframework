1f8bab2c6499cd147b034eb04ca6fe31d26b010a
Victor Catalán <donbungle@gmail.com>
2022-03-18 09:55:06 -0300

    Nueva vista y configuracion

--- backend/projapi/kanban/urls.py
+++ backend/projapi/kanban/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backend/projapi/projapi/settings.py
+++ backend/projapi/projapi/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-p1(1jx&vfzz!dcsl)qb@nxxy47a0q_wnsr+jv+=jwh5y6d^me)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.1.88',
+    'localhost'
+]
 
 
 # Application definition
@@ -141,7 +144,9 @@ CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:9111',
+    'http://192.168.1.88:9111',
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'http://localhost:9111',
+    'http://192.168.1.88:9111',
 ]
